d2746e77f3a6c340df2b4f8aa3d66e053c97e54b
Misbah-web192 <misbah.web192@gmail.com>
2026-04-28 10:11:58 -0800

    Button color change

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
         <div data-aos="fade-up"  data-aos-easing="ease-in-out"
           className="hero-1  w-[100%] lg:w-145 lg:ml-10  "><h2 className='font-sans font- text-5xl leading-normal text-[rgb(22,28,45)]'>We <span className='text-[rgb(109,199,122)]'> Geekinate</span> solutions for your digital needs</h2>
           <p className='text-lg tracking-wide pt-5 text-[rgb(137,150,169)]'>Geekinate is the process of bringing your digital vision to life with our team of Geeks.</p>
-          <button className='text-white mt-5 py-2.5 px-5 font-bold rounded-md shadow-md shadow-[rgb(213,237,219)] bg-[rgb(223,13,163)]' data-aos="fade-up" data-aos-delay="400" data-aos-easing="ease-in-out"
+          <button className='text-white mt-5 py-2.5 px-5 font-bold rounded-md shadow-md shadow-[rgb(213,237,219)] bg-[rgb(109,199,122)]' data-aos="fade-up" data-aos-delay="400" data-aos-easing="ease-in-out"
           >Let's Connect</button>
         </div>
         <div data-aos="fade-up" data-aos-easing="ease-in-out"
